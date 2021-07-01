08d3caee50c2a20d3c50e38e40cb5a0689f63ee2
dowap2 <dowapdowari@gmail.com>
2021-07-02 02:14:26 +0900

    add commnet

--- src/View/Comment.jsx
+++ src/View/Comment.jsx
@@ -1,37 +1,77 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+const CommentItem = styled.div``;
+
 const CommentComponent = styled.div`
+  box-sizing: border-box;
+  width: 980px;
+  margin: auto;
+  border: 1px solid #f1f1f1;
+  padding: 20px;
+`;
+
+const CommentInputComponent = styled.div`
   width: 980px;
   margin: auto;
 `;
 
 export function Comment(props) {
   const [comment, setComment] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   const [commentList, setCommentList] = useState([]);
   const [commentListItem, setCommentListItem] = useState([]);
 
   const Confirm = () => {
-    setCommentList(commentList.concat([comment]));
-    setComment("");
+    if (userName === "" || password === "") {
+      alert("user, password is null");
+    } else {
+      setCommentList(
+        commentList.concat([{ user: userName, comment: comment }])
+      );
+      setComment("");
+      setUserName("");
+      setPassword("");
+    }
   };
 
   useEffect(() => {
     setCommentListItem(
       commentList.map(list => {
-        return <div>{list}</div>;
+        return (
+          <CommentItem>
+            {list.user}: {list.comment}
+          </CommentItem>
+        );
       })
     );
   }, [commentList]);
   return (
-    <CommentComponent>
-      {commentListItem}
-      <input
-        type="text"
-        value={comment}
-        onChange={e => setComment(e.target.value)}
-      />
-      <button onClick={Confirm}>confirm</button>
-    </CommentComponent>
+    <div>
+      <button onClick={e => console.log(commentList)}></button>
+      <CommentComponent>{commentListItem}</CommentComponent>
+      <CommentInputComponent>
+        <input
+          type="text"
+          placeholder="comment"
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="user"
+          value={userName}
+          onChange={e => setUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button onClick={Confirm}>confirm</button>
+      </CommentInputComponent>
+    </div>
   );
 }
