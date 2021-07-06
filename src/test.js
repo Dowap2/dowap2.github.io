5ebdfb9a0bdcb0a07ae92516c65ef91f5202573a
dowap2 <dowapdowari@gmail.com>
2021-07-06 18:54:38 +0900

    add object === object

--- src/test.js
+++ src/test.js
@@ -1,5 +1,6 @@
 const foo = { a: 1, b: 2 };
 const bar = { a: 1, b: 2 };
-const clone = foo;
 
-console.log(Object.is(foo, clone));
+console.log(JSON.stringify(foo));
+console.log(JSON.stringify(bar));
+console.log(JSON.stringify(foo) === JSON.stringify(bar));
