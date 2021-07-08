f3643edbf9c6ec1dfb4d0a432d1861bdeef8ed86
dowap2 <dowapdowari@gmail.com>
2021-07-08 22:10:38 +0900

    banner image no repeat

--- src/Main/Banner.jsx
+++ src/Main/Banner.jsx
@@ -4,8 +4,11 @@ const BannerItem = styled.div`
   width: 100vw;
   height: 240px;
   color: #000;
+  background: #ffffff;
   background-image: ${props => `url(${props.background})`};
-  background-size: contain;
+  background-size: auto;
+  background-repeat: no-repeat;
+  background-position: center;
   display: flex;
   float: left;
   text-align: center;
