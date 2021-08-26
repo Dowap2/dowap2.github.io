13778474602acd9fa85ad8ddba550f9887ed5962
dowap2 <dowapdowari@gmail.com>
2021-08-26 21:14:38 +0900

    add comment output's styled

--- src/Blog/Main/View/Comment/CommentOutput.jsx
+++ src/Blog/Main/View/Comment/CommentOutput.jsx
@@ -2,14 +2,44 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 
-const CommentItem = styled.div``;
+const CommentItem = styled.div`
+  border-top: 1px solid #f5f5f5;
+  padding: 24px 0px;
+`;
+
+const CommentHeader = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CommentProfileImg = styled.div`
+  width: 54px;
+  height: 54px;
+  border: 1px solid #000;
+  border-radius: 100%;
+`;
+const CommentInfo = styled.div`
+  margin-left: 16px;
+`;
+const Name = styled.div`
+  font-weight: 800;
+`;
+const Date = styled.div`
+  font-size: 14px;
+  color: #868e96;
+  margin-top: 8px;
+`;
+
+const Comment = styled.div`
+  font-size: 18px;
+  font-weight: 400;
+  margin: 18px 0px;
+`;
 
 const CommentComponent = styled.div`
   box-sizing: border-box;
   width: 980px;
   margin: auto;
-  border: 1px solid #f1f1f1;
-  padding: 20px;
   box-sizing: border-box;
   @media only screen and (max-width: 960px) {
     width: 100%;
@@ -21,14 +51,22 @@ export function CommentOutput(props) {
   const [commentListItem, setCommentListItem] = useState([]);
 
   const commentList = commentState.comment[props.index];
-
+  const exampleDate = "2018년 11월 29일";
   useEffect(() => {
     if (commentList !== undefined) {
       setCommentListItem(
         commentList.map(list => {
           return (
             <CommentItem>
-              {list.user}: {list.comment}
+              <CommentHeader>
+                <CommentProfileImg />
+                <CommentInfo>
+                  <Name>{list.user}</Name>
+                  <Date>{exampleDate}</Date>
+                </CommentInfo>
+              </CommentHeader>
+
+              <Comment>{list.comment}</Comment>
             </CommentItem>
           );
         })
