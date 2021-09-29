47e77d4b7b200d8c0e4621975473080ee41f0713
dowap2 <dowapdowari@gmail.com>
2021-09-29 18:58:39 +0900

    add delete button style

--- src/Blog/Main/View/Comment/DeleteComponent.jsx
+++ src/Blog/Main/View/Comment/DeleteComponent.jsx
@@ -2,13 +2,24 @@ import styled from "styled-components";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
-const DeleteButton = styled.button``;
+const DeleteButton = styled.button`
+  width: 60px;
+  height: 20px;
+  background: red;
+  border: 0;
+  border-radius: 5px;
+  margin-left: 10px;
+`;
 const DeleteInput = styled.input``;
+const DeleteContainer = styled.div`
+  margin-left: auto;
+`;
 
 function DeleteComponent(props) {
   const [deletePassword, setDeletePassword] = useState("");
   const commentState = useSelector(state => state.commentState.state);
   const commentList = commentState.comment[props.blogNum];
+  const [isDeleteButtonView, setIsDeleteButtonView] = useState(false);
 
   const deleteFunc = (password, index) => {
     const obj = {};
@@ -20,16 +31,26 @@ function DeleteComponent(props) {
     }
   };
   return (
-    <div>
-      <DeleteInput
-        type="password"
-        value={deletePassword}
-        onChange={e => setDeletePassword(e.target.value)}
-      ></DeleteInput>
-      <DeleteButton
-        onClick={e => deleteFunc(props.password, props.index - 1)}
-      ></DeleteButton>
-    </div>
+    <DeleteContainer>
+      {isDeleteButtonView === false ? (
+        <DeleteButton onClick={e => setIsDeleteButtonView(!isDeleteButtonView)}>
+          삭제
+        </DeleteButton>
+      ) : (
+        <div>
+          <DeleteInput
+            type="password"
+            value={deletePassword}
+            onChange={e => setDeletePassword(e.target.value)}
+          ></DeleteInput>
+          <DeleteButton
+            onClick={e => deleteFunc(props.password, props.index - 1)}
+          >
+            삭제
+          </DeleteButton>
+        </div>
+      )}
+    </DeleteContainer>
   );
 }
 export default DeleteComponent;
